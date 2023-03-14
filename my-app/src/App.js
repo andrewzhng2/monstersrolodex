fcecad7d0de6d091dd4da71378d595a6de334fdf
andrewzhng2 <andrew.zhng2@gmail.com>
2023-03-14 13:43:03 -0400

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,27 +7,46 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Linda',
-          id: '1abc',
-        },
-        {
-          name: 'Frank',
-          id: '2abc',
-        },
-        {
-          name: 'Jacky',
-          id: '3abc',
-        },
-      ]
+      monsters: [],
+      searchField: '',
     };
   }
 
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users') 
+    .then((response) => response.json())
+    .then((users) => 
+      this.setState(
+        () => {
+          return { monsters: users };
+        },
+        () => {
+          console.log(this.state);
+        }
+      )
+    );
+  }
+
   render() {
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
+      <input 
+        className='search-box' 
+        type='search' 
+        placeholder='Search Monsters...' 
+        onChange={(event) => {
+          const searchField = event.target.value.toLocaleLowerCase();
+
+          this.setState(() => {
+            return { searchField }
+          });
+        }}
+      />
+        {filteredMonsters.map((monster) => {
           return <div key={monster.id}>
             <h1>{monster.name}</h1>
           </div>;
